feafb5ea5a9a5a811acac43805c7ad6c622c9b45
jafethmontero <jafethmontero@gmail.com>
2025-02-27 19:44:51 -0600

    skip optional dependencies in webpack to avoid warnings

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -20,4 +20,15 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "server.js",
   },
+  externals: {
+    express: "commonjs express",
+    kerberos: "commonjs kerberos",
+    "@mongodb-js/zstd": "commonjs @mongodb-js/zstd",
+    "@aws-sdk/credential-providers": "commonjs @aws-sdk/credential-providers",
+    "gcp-metadata": "commonjs gcp-metadata",
+    snappy: "commonjs snappy",
+    socks: "commonjs socks",
+    aws4: "commonjs aws4",
+    "mongodb-client-encryption": "commonjs mongodb-client-encryption",
+  },
 };
